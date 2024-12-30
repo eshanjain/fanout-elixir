01eb8a10ac75fcfac44d7dc56a96335c39ffa4ed
eshanjain <eshanjain1000@gmail.com>
2024-12-30 11:32:41 -0800

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -1,9 +1,9 @@
-defmodule MyProject.MixProject do
+defmodule YourApp.MixProject do
   use Mix.Project
 
   def project do
     [
-      app: :my_project,
+      app: :your_app_name,
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
@@ -11,5 +11,18 @@ defmodule MyProject.MixProject do
     ]
   end
 
-  # ... rest of the file
+  # Define any application configuration here
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Define your dependencies here
+  defp deps do
+    [
+      # Add your dependencies here, for example:
+      # {:some_dep, "~> 1.0"}
+    ]
+  end
 end
